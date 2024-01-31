d2028fe14bf5ae99c346ab4774e931d53c2cb966
samue <samue@Anonymous-x16.attlocal.net>
2024-01-31 14:17:34 -0600

    Commit 4

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -9,22 +9,24 @@ public class Ruin {
 		System.out.println("What is your starting amount?");
 		double startAmount = in.nextDouble();
 		System.out.println("What is the win probability in percent chance?");
-		double winChance = in.nextDouble();
+		double winChance = in.nextDouble()/100;
 		System.out.println("What is the limit of winnings?");
 		double winLimit = in.nextDouble();
-		double someChance = Math.random() * (0 + (1/(winChance)/100));
-		boolean winningChance = (someChance == 0);
+		double someChance = Math.random() * (0 + 1);
+		
 		String win = "";
-		if (winningChance = false); {
+		while (startAmount > 0) 
+		if (someChance > winChance) {
 			win = "You lost";
+			startAmount = startAmount - 1; 
 		
-		} System.out.println(win); 
-			
-		
-		
-			
+		} 
+		else {
+			win = "You win";
+			startAmount = startAmount + 1;
 		}
 		
+		System.out.println(win);
 		
 		
 		
