b054aa3a46bb573417ca3171c0c29b8258e3df8f
samue <samue@Anonymous-x16.attlocal.net>
2024-01-31 13:32:31 -0600

    2nd commit

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -1,9 +1,18 @@
 package studio2;
-
+import java.util.Scanner;
 public class Ruin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("What is your starting amount?");
+		double startAmount = in.nextDouble();
+		System.out.println("What is the win probability?");
+		double winChance = in.nextDouble();
+		System.out.println("What is the limit of winnings?");
+		double winLimit = in.nextDouble();
+		
+		
+		S
 
 	}
 
